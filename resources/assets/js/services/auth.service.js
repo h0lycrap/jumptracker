e937c9dc442163ae644e6c7d1e8e7fc5dfee2979
Gabriel <gabachour44@gmail.com>
2020-08-04 23:19:49 -0400

    setup for heroku

--- resources/assets/js/services/auth.service.js
+++ resources/assets/js/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://urtjumptracker.herokuapp.com/api/auth/';//
+const API_URL = 'http://urtjumptracker.herokuapp.com/api/auth/';//
 
 class AuthService {
   login(user) {
--- resources/assets/js/services/user.service.js
+++ resources/assets/js/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'https://urtjumptracker.herokuapp.com/api/auth/';
+const API_URL = 'http://urtjumptracker.herokuapp.com/api/auth/';
 
 class UserService {
 
